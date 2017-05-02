5b263f1d124accb2e4c546821e6ac47f330eb045
WillRatterman <will.ratterman@gmail.com>
2017-05-02 09:09:53 -0600

    initial commit

---
+++ pseudocode.rb
@@ -0,0 +1,36 @@
+ready_to_quit = false
+
+puts "Hello, welcome to Will's homework Assignment number 1."
+
+print "what would you like me to call you by?"
+
+# Use 'gets' to get user value set to variable 'name'
+
+until ready_to_quit
+  "Well, #{name} I am Mark. What can I help you with?"
+
+  # use 'gets' to get user input value set to the variable answer
+
+  # if 'answer' is equal to '' or nothing entered, the program will
+  # respond "HELLO?"
+
+  # elsif the value of 'answer' is all lowercase, the program will
+  # respond "I am having a difficult time hearing you"
+
+
+  # elsif the value of 'answer' is all upper case, the program will
+  # respond "No, this is not a pet store."
+
+
+  # elsif answer is 'Goodbye', then the program responds...
+  # ..."Is there anything else I can gelp you with?"
+
+      # use gets to get user input, set to value 'final'
+      # if 'final' == "Goodbye", the program will close
+
+      # else, the program will respond
+    end
+  # else the program will look up the correct response to 'answer'
+end
+
+puts "THANK YOU FOR CALLING!"
